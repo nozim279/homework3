e2f44f9054ab7d9632965678c2c99a9e2cefc575
nozim279 <n.tolibov@newuu.uz>
2024-09-30 15:58:19 +0500

    problem3.cpp

---
+++ problem3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int N, A, B;
+    cout << "Enter three numbers for N, A, B: ";
+    cin >> N >> A >> B;
+
+    int totalSum = 0;
+
+    for (int i = 1; i <= N; ++i) {
+        int digitSum = sumOfDigits(i);
+        if (digitSum >= A && digitSum <= B) {
+            totalSum += i;
+        }
+    }
+
+    cout << totalSum << endl;
+    return 0;
+}
